7fca80a527a834f30bb784eac9251294ed6505a0
LettuceLizard <you@example.com>
2024-02-08 17:24:52 +0100

    trying to get pipeline to work

---
+++ cypress/integration/simple_spec.js
@@ -0,0 +1,6 @@
+// cypress/integration/simple_spec.js
+describe('The Main Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/') // Adjust this URL to your application's main page
+  })
+})
