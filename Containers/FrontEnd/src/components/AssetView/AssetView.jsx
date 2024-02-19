1ea121ee32e474dff7f65ac8bd6a16b9a37f8439
HUGO NORDENSTRÖM <huno20@student.bth.se>
2024-02-19 14:46:11 +0000

    F014: add asset button + form

--- Containers/FrontEnd/src/components/AssetView/AssetView.jsx
+++ Containers/FrontEnd/src/components/AssetView/AssetView.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import './AssetView.css'
 
 
 export default function AssetView() {
     const postData = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!postData.state) {
+            navigate('/');
+        }
+    }, [postData.state, navigate]);
+
+    if (!postData.state) {
+        return <div>Loading...</div>; // or null, or any placeholder you prefer
+    }
+
     return (
         <div className='asset-view-container'>
             <div className='asset-info-container'>
