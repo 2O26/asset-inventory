d788f28ee5de7e262713a0137bc97c09e0518e94
Loadeumn <hugonordenstrom@hotmail.com>
2024-02-20 10:22:59 +0100

    NOID: Moved and updated Template Strucutres

---
+++ CyprestTests/cypress/e2e/navbar.cy.js
@@ -0,0 +1,50 @@
+describe('Navbar tests', () => {
+  beforeEach('visiting homepage', () => {
+    cy.visit(Cypress.env('baseUrl'))
+  })
+
+  describe('when visiting dashboard page', () => {
+    beforeEach('visiting dashboard', () => {
+      cy.contains('Dashboard').click()
+    })
+    it('can verify correct url', () => {
+      cy.url().should('eq', Cypress.env('baseUrl'))
+    })
+  })
+
+  describe('when visiting tools page', () => {
+    beforeEach('visiting tools', () => {
+      cy.contains('Tools').click()
+    })
+    it('can verify correct url', () => {
+      cy.url().should('eq', `${Cypress.env('baseUrl')}tools`)
+    })
+  })
+
+  describe('when visiting settings page', () => {
+    beforeEach('visiting settings', () => {
+      cy.contains('Settings').click()
+    })
+    it('can verify correct url', () => {
+      cy.url().should('eq', `${Cypress.env('baseUrl')}settings`)
+    })
+  })
+
+  describe('when visiting profile page', () => {
+    beforeEach('visiting profile', () => {
+      cy.contains('Profile').click()
+    })
+    it('can verify correct url', () => {
+      cy.url().should('eq', `${Cypress.env('baseUrl')}profile`)
+    })
+  })
+
+  describe('when visiting signin page', () => {
+    beforeEach('visiting signin', () => {
+      cy.contains('Sign in').click()
+    })
+    it('can verify correct url', () => {
+      cy.url().should('eq', `${Cypress.env('baseUrl')}signin`)
+    })
+  })
+})
