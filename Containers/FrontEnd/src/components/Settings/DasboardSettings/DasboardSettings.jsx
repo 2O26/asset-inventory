d82d9958fbf4555137f31f8346bad5c9ced0ddd3
Loadeumn <77837153+Loadeumn@users.noreply.github.com>
2024-03-20 20:23:58 +0100

    F069 (#10)
    * Added magnifying glass icon to search bar
    
    * F066: Styled setting page
    
    ---------
    
    Co-authored-by: moondog <makv20@student.bth.se>

---
+++ Containers/FrontEnd/src/components/Settings/DasboardSettings/DasboardSettings.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './DasboardSettings.css'
+
+export default function DasboardSettings() {
+    return (
+        <div>Dasboard Settings</div>
+    )
+}
--- Containers/FrontEnd/src/components/Settings/Settings.jsx
+++ Containers/FrontEnd/src/components/Settings/Settings.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SetNetworkScanSettings from './SetNetworkScanSettings/SetNetworkScanSettings'
+import DasboardSettings from './DasboardSettings/DasboardSettings'
+import './Settings.css'
+
+const settingsDict = {
+    "Network Scan Settings": <SetNetworkScanSettings />,
+    "Dashboard Settings": <DasboardSettings />
+};
 
 export default function Settings() {
+    const [selectedView, setSelectedView] = useState('Information');
+    // Initialize visibility states for each section based on settingsDict
+    const [visibilityStates, setVisibilityStates] = useState(
+        Object.keys(settingsDict).reduce((acc, key) => {
+            acc[key] = false; // Start with all sections closed
+            return acc;
+        }, {})
+    );
+
+    // Function to toggle visibility of a section
+    const toggleVisibility = (section) => {
+        setVisibilityStates(prevState => ({
+            ...prevState,
+            [section]: !prevState[section]
+        }));
+    };
+
     return (
-        <div>
-            <div>
-                <h1 style={{ color: "var(--text-color)", marginTop: "4rem" }}>Settings</h1>
-            </div>
-            <div className='netScanConfig'>
-                <SetNetworkScanSettings />
-            </div>
+        <div className='center-flex-column'>
+            {Object.entries(settingsDict).map(([key, component]) => (
+                <div key={key}>
+                    <div
+                        className={visibilityStates[key] ? "settings-header show-content" : "settings-header"}
+                        onClick={() => toggleVisibility(key)}
+                        style={{ cursor: 'pointer', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                    >
+                        {key}
+                        <button className='arrow-container'>
+                            {visibilityStates[key] ? <i className="arrow down"></i> : <i className="arrow up"></i>}
+                        </button>
+                    </div>
+                    {visibilityStates[key] && (
+                        <div className='settings-container'>
+                            {component}
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
+
