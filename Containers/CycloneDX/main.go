7bc74e7cd3091d4871e657893a5613c86b74d80a
daniel <danieltagegustav@gmail.com>
2024-03-27 16:24:44 +0100

    added CycloneDX container

---
+++ Containers/CycloneDX/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	dbcon "assetinventory/cyclonedx/dbcon-cyclonedx"
+	"bytes"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+)
+
+func uploadCycloneDX(c *gin.Context) {
+	// Limit the size of the request body to 10MB
+	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 10<<20)
+
+	// Retrieve the file from the form data
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid file upload attempt",
+		})
+		return
+	}
+
+	if file.Header.Get("Content-Type") != "application/json" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Only JSON files are allowed",
+		})
+		return
+	}
+	// Fetch the assetID from the POST request
+	assetID := c.PostForm("assetID")
+
+	// Log the file information
+	fmt.Printf("Uploaded File: %+v\n", file.Filename)
+	fmt.Printf("File Size: %+v\n", file.Size)
+	fmt.Printf("MIME Header: %+v\n", file.Header)
+	fmt.Printf("Asset ID: %s\n", assetID) // Print the assetID
+
+	// Create a new file in the current working directory
+	//dst := "./" + file.Filename
+	//if err := c.SaveUploadedFile(file, dst); err != nil {
+	//c.JSON(http.StatusInternalServerError, gin.H{
+	//	"error": "Failed to save the file",
+	//})
+	//return
+	//}
+	// Read the file content into a byte slice
+	fileContent, err := file.Open()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to open the file",
+		})
+		return
+	}
+	defer fileContent.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, fileContent); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to read the file",
+		})
+		return
+	}
+	// Save the SBOM data to the database
+	sbomData := buf.Bytes()
+	db := &dbcon.MongoDBHelper{Collection: dbcon.GetCollection("SBOMS")}
+	err = dbcon.SaveCycloneDX(db, sbomData, assetID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to save SBOM to database",
+		})
+		return
+	}
+	// Respond to the clientS
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("File uploaded successfully: %s", file.Filename),
+	})
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	}
+}
+
+func main() {
+
+	router := gin.Default()
+	// Apply the CORS middleware
+	router.Use(CORSMiddleware())
+	router.POST("/uploadCycloneDX", uploadCycloneDX)
+	err := dbcon.SetupDatabase("mongodb://cyclonedxstorage:27020/", "SBOM")
+	if err != nil {
+		log.Fatalf("Could not set up database: %v", err)
+	}
+
+	//scansHelper := &dbcon.MongoDBHelper{Collection: dbcon.GetCollection("SBOMS")}
+	log.Println("Server starting on port 8082...")
+	if err := router.Run(":8082"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+}
