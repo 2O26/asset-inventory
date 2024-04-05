90e238d2f184bb3fee2a7b33823fbd003438658b
Loadeumn <hugonordenstrom@hotmail.com>
2024-04-05 19:57:36 +0200

    F083: CycloneDX tab + logic

--- Containers/FrontEnd/src/components/AssetView/AssetView.jsx
+++ Containers/FrontEnd/src/components/AssetView/AssetView.jsx
@@ -7,8 +7,9 @@ import './AssetView.css';
 import { AssetInfo } from './AssetInfo';
 import GraphView from '../Tools/GraphView/GraphView';
 import EditAsset from './EditAsset';
-import {LeftArrow, RightArrow} from '../common/Icons/Icons';
+import { LeftArrow, RightArrow } from '../common/Icons/Icons';
 import { CSSTransition } from 'react-transition-group';
+import CycloneDX from './CycloneDX';
 
 
 export default function AssetView() {
@@ -52,56 +53,69 @@ export default function AssetView() {
     // if (!data.state.assets[assetID]) return <div>No asset data!</div>;
 
 
-    
+
     return (
-         <div className='asset-view-container'>
-             <GraphView selectedAsset={assetID} />
-     
-             <button
-                 className="toggle-button"
-                 onClick={() => setIsExpanded(!isExpanded)}
-             >
-                 {isExpanded ? <RightArrow /> : <LeftArrow />}
-             </button>
-             <CSSTransition //This code is here to support the asset view container sliding smoothly away to the right, but it's
-                 in={isExpanded && !!data.state.assets[assetID]} //disabled for now because I couldn't get it to work -Karlsson.
-                 timeout={0} //Delay in milliseconds.
-                 classNames="slide"
-                 unmountOnExit
-             >
-                 <div className="button-plus-info">
-                     <div className="button-container">
-                         <button
-                             className={`tab-button ${selectedView === 'Information' ? 'active-button' : ''}`}
-                             onClick={() => handleButtonClick('Information')}
-                         >
-                             Information
-                         </button>
-                         <button
-                             className={`tab-button ${selectedView === 'History' ? 'active-button' : ''}`}
-                             onClick={() => handleButtonClick('History')}
-                         >
-                             History
-                         </button>
-                         <button
-                             className={`tab-button ${selectedView === 'Edit' ? 'active-button' : ''}`}
-                             onClick={() => handleButtonClick('Edit')}
-                         >
-                             Edit
-                         </button>
-                     </div>
-                     {/* Conditionally render content based on selectedView */}
-                     {selectedView === 'Information' && (
-                         <div>
-                             <AssetInfo data={data} assetID={assetID} showPluginInfo={true} ></AssetInfo>
-                         </div>)}
-                     {selectedView === 'History' && (
-                         <AssetInfo data={data} assetID={assetID} title={"History Page"} showPluginInfo={false}></AssetInfo>)}
-                     {selectedView === 'Edit' && (
-                         <EditAsset assetData={data.state.assets[assetID]} assetID={assetID} relationData={filteredRelations} refetch={refetch}></EditAsset>)}
- 
- 
-                 </div>
-             </CSSTransition>
-         </div>
-     );}
+        <div className='asset-view-container'>
+            <GraphView selectedAsset={assetID} />
+
+            <button
+                className="toggle-button"
+                onClick={() => setIsExpanded(!isExpanded)}
+            >
+                {isExpanded ? <RightArrow /> : <LeftArrow />}
+            </button>
+            <CSSTransition //This code is here to support the asset view container sliding smoothly away to the right, but it's
+                in={isExpanded && !!data.state.assets[assetID]} //disabled for now because I couldn't get it to work -Karlsson.
+                timeout={0} //Delay in milliseconds.
+                classNames="slide"
+                unmountOnExit
+            >
+                <div className="button-plus-info">
+                    <div className="button-container">
+                        <button
+                            className={`tab-button ${selectedView === 'Information' ? 'active-button' : ''}`}
+                            onClick={() => handleButtonClick('Information')}
+                        >
+                            Information
+                        </button>
+                        <button
+                            className={`tab-button ${selectedView === 'History' ? 'active-button' : ''}`}
+                            onClick={() => handleButtonClick('History')}
+                        >
+                            History
+                        </button>
+                        <button
+                            className={`tab-button ${selectedView === 'Edit' ? 'active-button' : ''}`}
+                            onClick={() => handleButtonClick('Edit')}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            className={`tab-button ${selectedView === 'CycloneDx' ? 'active-button' : ''}`}
+                            onClick={() => handleButtonClick('CycloneDx')}
+                        >
+                            C-DX
+                        </button>
+                    </div>
+                    {/* Conditionally render content based on selectedView */}
+                    {selectedView === 'Information' && (
+                        <div>
+                            <AssetInfo data={data} assetID={assetID} showPluginInfo={true} ></AssetInfo>
+                        </div>)
+                    }
+                    {selectedView === 'History' && (
+                        <AssetInfo data={data} assetID={assetID} title={"History Page"} showPluginInfo={false}></AssetInfo>)
+                    }
+                    {selectedView === 'Edit' && (
+                        <EditAsset assetData={data.state.assets[assetID]} assetID={assetID} relationData={filteredRelations} refetch={refetch}></EditAsset>)
+                    }
+                    {selectedView === 'CycloneDx' && (
+                        <CycloneDX assetID={assetID} ></CycloneDX>)
+                    }
+
+
+                </div>
+            </CSSTransition>
+        </div>
+    );
+}
