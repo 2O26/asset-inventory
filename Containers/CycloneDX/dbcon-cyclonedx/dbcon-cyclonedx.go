75a7d41c19918c44ce1e0e5432c3db93d0231aeb
Elliot Marcusson <elmr20@student.bth.se>
2024-04-05 11:43:24 +0000

    update

--- Containers/CycloneDX/dbcon-cyclonedx/dbcon-cyclonedx.go
+++ Containers/CycloneDX/dbcon-cyclonedx/dbcon-cyclonedx.go
@@ -63,7 +63,7 @@ func GetCollection(collectionName string) *mongo.Collection {
 func ScanAndSaveCVEs(assetID string, sbomFilePath string) {
 	outputFilePath := fmt.Sprintf("cve-results-%s.json", assetID)
 
-    cmd := exec.Command("cve-bin-tool", "--sbom", "cyclonedx", "--sbom-file", "sbom.json", "-f", "json", "-o", outputFilePath)
+    cmd := exec.Command("python", "-m", "cve_bin_tool.cli", "--sbom", "cyclonedx", "--sbom-file", "sbom.json", "-f", "json", "-o", outputFilePath)
 	startTime := time.Now()
 	fmt.Printf("Running command: %s\n", strings.Join(cmd.Args, " "))
 
