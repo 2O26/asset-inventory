df15601f27691c9cb90b33faf254f67f4e400d26
MarKick <57588039+MarKick@users.noreply.github.com>
2024-04-05 09:49:38 +0200

    F064.5: Fetching dashboard from config (#41)
    Co-authored-by: Marcus Kicklighter <makk20@student.bth.se>

--- Containers/FrontEnd/src/components/Dashboard/Dashboard.jsx
+++ Containers/FrontEnd/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Dashboard.css'
 import { dashboardTools } from '../Tools/Tools.jsx'
-
-const tools75by50 = ["Graph View", "Asset List", "Graph View"]
-const tools25by50 = ["Asset List", "Graph View", "Asset List"]
+import { SaveUserSetting, GetUserSettings } from '../Services/ApiService.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 export default function Dashboard() {
+    const [tools25by50, setTools25by50] = useState(["Asset List", "Graph View", "Asset List"]);
+    const [tools75by50, setTools75by50] = useState(["Graph View", "Asset List", "Graph View"]);
+
+    const { mutate, isPending, isError, error } = useMutation({
+        mutationFn: SaveUserSetting,
+        onSuccess: (data) => {
+            // window.alert("Settings Saved")
+            refetchUserSettings();
+        },
+        onError: (error) => {
+            console.error("Save Dashboard Config: ", error);
+        }
+    });
+
+    const { data: userSettingData, isLoading: isLoadingUserSettings, isError: isUserSettings, error: errorUserSettings, refetch: refetchUserSettings } = useQuery({
+        queryKey: ['User Settings'],
+        queryFn: GetUserSettings,
+        enabled: true
+    });
+
+    useEffect(() => {
+        if (userSettingData) {
+            setTools25by50(userSettingData.userSettings[0].rightDash);
+            setTools75by50(userSettingData.userSettings[0].leftDash);
+        }
+    }, [userSettingData])
+
     return (
         <div className='dashboard-container' >
             <div className='container-75-50'>
