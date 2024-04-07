eb29fd4248e9ee6a4f099ed3698c7a863eefbed6
Loadeumn <77837153+Loadeumn@users.noreply.github.com>
2024-04-07 16:03:11 +0200

    Admin console tool added (#66)

--- Containers/FrontEnd/src/components/common/Icons/Icons.jsx
+++ Containers/FrontEnd/src/components/common/Icons/Icons.jsx
@@ -17,15 +17,24 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import { MdCyclone } from "react-icons/md";
 import { BsFiletypePdf } from "react-icons/bs";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
+
 
 
 // get icons from HERE: https://react-icons.github.io/react-icons/
+
+export const AdminConsoleIcon = ({ size = 30 }) => {
+    return (
+        <MdOutlineAdminPanelSettings size={size} />
+    )
+}
 
 export const CycloneDXIcon = ({ size = 30 }) => {
     return (
         <MdCyclone size={size} />
     )
 }
+
 export const LeftArrow = ({ size = 30 }) => {
     return (
         <IoIosArrowBack size={size} />
