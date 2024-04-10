2d5d8bbd6b1aee83bea8276591f83386f7669409
Elliot Marcusson <elmr20@student.bth.se>
2024-04-10 14:28:41 +0000

    newline

--- Containers/AssetHandler/dbcon/dbcon.go
+++ Containers/AssetHandler/dbcon/dbcon.go
@@ -703,6 +703,7 @@ func DeleteAllDocuments(db DatabaseHelper, c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Documents deleted", "count": deleteResult.DeletedCount})
 }
+
 func GetTimelineData(db DatabaseHelper, c *gin.Context) {
 	ctx := context.TODO()
 	assetID := c.Query("assetID")
