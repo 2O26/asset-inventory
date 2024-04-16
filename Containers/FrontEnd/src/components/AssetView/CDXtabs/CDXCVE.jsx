70b028fb0f89b832482d4ec5379483e3a8065083
MarKick <57588039+MarKick@users.noreply.github.com>
2024-04-16 09:53:04 +0200

    F086: Updated vulnerble CVEs page to match new backend data (#99)
    Co-authored-by: Marcus Kicklighter <makk20@student.bth.se>

--- Containers/FrontEnd/src/components/AssetView/CDXtabs/CDXCVE.jsx
+++ Containers/FrontEnd/src/components/AssetView/CDXtabs/CDXCVE.jsx
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import './CDXdata.css';
-import { JSONTree } from 'react-json-tree';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { GetVulnerbleComponents } from '../../Services/ApiService';
 import LoadingSpinner from '../../common/LoadingSpinner/LoadingSpinner';
 import { StatusIcon } from '../../common/Icons/Icons';
-const theme = {
-    base00: 'var(--base-00)',
-    base01: 'var(--base-01)',
-    base02: 'var(--base-02)',
-    base03: 'var(--base-03)',
-    base04: 'var(--base-04)',
-    base05: 'var(--base-05)',
-    base06: 'var(--base-06)',
-    base07: 'var(--base-07)',
-    base08: 'var(--base-08)',
-    base09: 'var(--base-09)',
-    base0A: 'var(--base-0A)',
-    base0B: 'var(--base-0B)',
-    base0C: 'var(--base-0C)',
-    base0D: 'var(--base-0D)',
-    base0E: 'var(--base-0E)',
-    base0F: 'var(--base-0F)',
-};
+import { SearchIcon } from '../../common/Icons/Icons';
+
+export function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (event) => {
+        const term = event.target.value;
+        setSearchTerm(term);
+        onSearch(term);
+    };
+
+    return (
+        <div className="SearchBar">
+            <div className="searchIconWrapper">
+                <SearchIcon size={30} />
+            </div>
+            <div className="inputWrapper">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                    placeholder="Search..."
+                />
+            </div>
+        </div>
+    );
+}
 
 export const CDXCVE = ({ assetID }) => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -44,25 +52,17 @@ export const CDXCVE = ({ assetID }) => {
 
     useEffect(() => {
         if (CVEData) {
-
-            // const newFilteredComponents = searchTerm === '' ? components.data :
-            //     components.data.filter(component => {
-            //         // Assuming 'name' and 'description' are searchable fields.
-            //         // You can add more fields to check as needed.
-            //         return component.type === "library" &&
-            //             (component.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            //                 component.description.toLowerCase().includes(searchTerm.toLowerCase()));
-            //     });
             if (CVEData.cycloneDXvulns) {
-                setFilteredfilteredLibraries(CVEData.cycloneDXvulns);
+                const newFilteredComponents = searchTerm === '' ? CVEData.cycloneDXvulns :
+                    CVEData.cycloneDXvulns.filter(component => {
+                        // Assuming 'name' is searchable fields.
+                        return (component.name.toLowerCase().includes(searchTerm.toLowerCase()));
+                    });
+                setFilteredfilteredLibraries(newFilteredComponents);
             }
 
         }
     }, [CVEData, searchTerm]);
-
-    function handleClick() {
-        console.log(CVEData.cycloneDXvulns.npm.vulnerabilities)
-    }
 
     function consoleVulnerbleLibraries() {
         if (filteredLibraries) {
@@ -93,44 +93,51 @@ export const CDXCVE = ({ assetID }) => {
                 {filteredLibraries && (
                     <button className='standard-button' onClick={() => consoleVulnerbleLibraries()}> Console vulns for asset</button>
                 )}
-                {filteredLibraries.vulnerabilities && (
-                    <div className='center-flex-column'>
-                        {Object.keys(filteredLibraries.vulnerabilities).map((key, index) => {
-                            const sevScale = { "critical": "#FF0000", "high": "#FFA500", "moderate": "#FFEA00", "low": "0F0" }
-                            const severity = filteredLibraries.vulnerabilities[key].severity;
-                            return (
-                                <div key={index} className='json-tree-container'>
-                                    <div
-                                        className={visibilityStates[index] ? "drop-down-header show-content" : "drop-down-header"}
-                                        onClick={() => toggleVisibility(index)}
-                                        style={{ cursor: 'pointer' }}>
-                                        {/* Assuming the 'name' property exists within the vulnerability details object */}
-                                        <div />
-                                        <strong>{filteredLibraries.vulnerabilities[key].name}</strong>
-                                        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                                            <div style={{ marginRight: "0.2rem" }}>
-                                                <StatusIcon size={15} color={sevScale[severity]} />
-                                            </div>
-                                            <button className='arrow-container'>
-                                                {visibilityStates[index] ? <i className="arrow down"></i> : <i className="arrow up"></i>}
-                                            </button>
-                                        </div>
-                                    </div>
-                                    {visibilityStates[index] && (
-                                        <div className='settings-container'>
-                                            {/* Here 'key' is used directly, as it is the unique identifier of the vulnerability */}
-                                            <p> Name: {filteredLibraries.vulnerabilities[key].name} </p>
-                                            {/* Accessing the 'severity' property from the vulnerability details object */}
-                                            <p> Severity: {severity} </p>
-                                        </div>
-                                    )}
-                                </div>
-                            )
-                        })}
-                    </div>
-                )}
-
                 <hr />
+                <div>
+                    <SearchBar onSearch={setSearchTerm} />
+                    {filteredLibraries && (
+                        <div className='center-flex-column'>
+                            {Object.keys(filteredLibraries).map((key, index) => {
+                                const sevScale = { "critical": "#FF0000", "high": "#FFA500", "moderate": "#FFEA00", "low": "0F0" }
+                                const severity = filteredLibraries[key].severity;
+                                return (
+                                    <div key={index} className='json-tree-container'>
+                                        <div
+                                            className={visibilityStates[index] ? "drop-down-header show-content" : "drop-down-header"}
+                                            onClick={() => toggleVisibility(index)}
+                                            style={{ cursor: 'pointer' }}>
+                                            {/* Assuming the 'name' property exists within the vulnerability details object */}
+                                            <div />
+                                            <strong>{filteredLibraries[key].name}</strong>
+                                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                                                <div style={{ marginRight: "0.2rem" }}>
+                                                    <StatusIcon size={15} color={sevScale[severity]} />
+                                                </div>
+                                                <button className='arrow-container'>
+                                                    {visibilityStates[index] ? <i className="arrow down"></i> : <i className="arrow up"></i>}
+                                                </button>
+                                            </div>
+                                        </div>
+                                        {visibilityStates[index] && (
+                                            <div className='settings-container'>
+                                                {/* Here 'key' is used directly, as it is the unique identifier of the vulnerability */}
+                                                <p> Library name: {filteredLibraries[key].name} </p>
+                                                {/* Accessing the 'severity' property from the vulnerability details object */}
+                                                <p> Severity: {severity} </p>
+                                                <p>CVE: {filteredLibraries[key].CVE[0].cve}</p>
+                                                <p>CVE score: {filteredLibraries[key].CVE[0].cvssScore}</p>
+                                                <p>Description: {filteredLibraries[key].CVE[0].description}</p>
+
+                                            </div>
+                                        )}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )}
+                </div>
+
             </div>
         </div>
     );
