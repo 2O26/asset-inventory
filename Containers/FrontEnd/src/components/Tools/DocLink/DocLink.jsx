83ec93189bc0f2d79ea1c0d9990458523379bbe6
moondog <markuskarlsson2001@gmail.com>
2024-04-18 16:19:44 +0200

    Doc Link implemented

--- Containers/FrontEnd/src/components/Tools/DocLink/DocLink.jsx
+++ Containers/FrontEnd/src/components/Tools/DocLink/DocLink.jsx
@@ -4,10 +4,14 @@ import { GetOSSAPIkey2 } from '../../Services/ApiService';
 export const RedirectToDocumentation = async () => {
     try {
         const resData = await GetOSSAPIkey2(); // Call the function to get the link
-        const link = resData.apikey; // Assuming 'link' is the key in resData containing the URL
+        const link = resData.apikey; // Assuming 'apikey' is the key in resData containing the URL
         window.open(link, '_blank'); // Open the link in a new tab
     } catch (error) {
         console.error(error);
         // Handle error appropriately, e.g., display an error message
+
+        // Fallback to a default link if fetching the API key fails
+        const fallbackLink = 'https://github.com/assetinventory-org/asset-inventory';
+        window.open(fallbackLink, '_blank'); // Open the fallback link in a new tab
     }
 };
