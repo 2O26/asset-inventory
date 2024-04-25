396ebc08cba47ee1291bec6c567284c036cae145
Loadeumn <77837153+Loadeumn@users.noreply.github.com>
2024-04-25 16:15:20 +0200

    F110.6: dint want to get merge conflicts (#143)

---
+++ Containers/Cypress/cypress/e2e/help.cy.js
@@ -0,0 +1,21 @@
+describe('Testing hepl button', () => {
+    beforeEach('loggin in and create ip range', () => {
+        cy.login();
+    })
+
+    afterEach('remove ip range and logout', () => {
+        cy.logout();
+    })
+
+    it("can redirect to help page", () => {
+        cy.window().then((win) => {
+            cy.stub(win, 'open').as('windowOpen');
+        });
+
+        // Perform the action that triggers the window.open
+        cy.contains('Help').click()
+
+        // Assert that window.open was called with the correct arguments
+        cy.get('@windowOpen').should('be.calledWith', "http://localhost:6880/home/", "_blank");
+    })
+})
